eef8d32e37fd5db1d1d85111bb6a7198fee39fe3
Ramanabhai <ramanamathi2003@gmail.com>
2025-08-11 19:54:53 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,30 @@
-import { useNavigate } from 'react-router-dom';
-
-
+import { useNavigate } from "react-router-dom";
+import "../index.css";
+import bgImage from "../assets/home.jpg";
 
 export default function Home() {
   const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>This is the protected Home page.</p>
-        <button onClick={() => navigate('/products')}>
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="home-content">
+        <h1 className="home-caption">
+          Celebrate Every Moment with Elegant Gold Covering Designs
+        </h1>
+        <p className="home-subtext">
+          Crafted to perfection, bringing you timeless beauty at an affordable price.
+        </p>
+        <button
+          className="view-products-btn"
+          onClick={() => navigate("/products")}
+        >
           View Products
         </button>
+      </div>
     </div>
   );
 }
+
